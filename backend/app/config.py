f5f36648d7af941d445f522c8c5ca9d988637f18
Kaan YASSIBAŞ <kyassibas@gmail.com>
2026-07-01 20:57:05 +0300

    Read environment from ENVIRONMENT variable

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,10 +1,12 @@
+import os
+
 from pydantic import BaseModel
 
 
 class Settings(BaseModel):
     app_name: str = "DeployBoard API"
     app_version: str = "0.1.0"
-    environment: str = "development"
+    environment: str = os.getenv("ENVIRONMENT", "development")
 
 
 settings = Settings()
